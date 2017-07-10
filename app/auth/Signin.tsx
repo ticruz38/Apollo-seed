128dee0944b7d12860571feddfaddbaf465d436a
Ticruz <thib.duchene@gmail.com>
2017-07-10 18:15:18 +0200

    playing with redux-form

--- app/auth/Signin.tsx
+++ app/auth/Signin.tsx
@@ -1,1 +1,32 @@
 import * as React from 'react'
+
+import { reduxForm, FormProps } from 'redux-form';
+
+
+
+
+export class Signin extends React.Component< FormProps<FormData any, any >, any > {
+
+
+    render(): React.ReactElement< * > {
+        return (
+            <form className="signin">
+
+            </form>
+        );
+    }
+}
+
+
+const validate = values => {
+    const errors = {};
+    if (!values.username) errors["username"] = "Required";
+    else if ( values.username.length > 15 ) errors["username"] = "Must be 15 characters or less";
+
+}
+
+
+export default reduxForm({
+    form: 'signin',
+    validate
+})(Signin)
